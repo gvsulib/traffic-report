beb20c574ff6e784538abb4059d97206add616e2
Jon Bloom <git@jon.tw>
2015-02-10 14:48:53 -0500

    changes

--- php/avgTraffic.php
+++ php/avgTraffic.php
@@ -23,19 +23,21 @@ traffic_labels la,
 	) a
 WHERE
 e.entryId = t.entryId";
-if ($_GET['include']){
-	for ($i = 0; $i < count($_GET['include']['begin']); $i++){
-		if (!($_GET['include']['begin'][$i] == "" || $_GET['include']['begin'][$i] == "")){
-			$q .= ("
-				AND e.time BETWEEN TIMESTAMP('" . $_GET['include']['begin'][$i] . "') AND TIMESTAMP('" . $_GET['include']['end'][$i] . "')");
+if ($_GET){
+	if ($_GET['include'] && $_GET['include']){
+		for ($i = 0; $i < count($_GET['include']['begin']); $i++){
+			if (!($_GET['include']['begin'][$i] == "" || $_GET['include']['begin'][$i] == "")){
+				$q .= ("
+					AND e.time BETWEEN TIMESTAMP('" . $_GET['include']['begin'][$i] . "') AND TIMESTAMP('" . $_GET['include']['end'][$i] . "')");
+			}
 		}
 	}
-}
-if ($_GET['exclude']){
-	for ($i = 0; $i < count($_GET['exclude']['begin']); $i++){
-		if (!($_GET['exclude']['begin'][$i] == "" || $_GET['exclude']['begin'][$i] == "")){
-		$q .= ("
-			AND e.time NOT BETWEEN TIMESTAMP('" . $_GET['exclude']['begin'][$i] . "') AND TIMESTAMP('" . $_GET['exclude']['end'][$i] . "')");
+	if ($_GET['exclude'] && $_GET['exclude']){
+		for ($i = 0; $i < count($_GET['exclude']['begin']); $i++){
+			if (!($_GET['exclude']['begin'][$i] == "" || $_GET['exclude']['begin'][$i] == "")){
+			$q .= ("
+				AND e.time NOT BETWEEN TIMESTAMP('" . $_GET['exclude']['begin'][$i] . "') AND TIMESTAMP('" . $_GET['exclude']['end'][$i] . "')");
+			}
 		}
 	}
 }
@@ -46,7 +48,7 @@ AND t.space = a.space
 AND la.id = a.rounded
 GROUP BY
 t.space";
-
+$data;
 $db_result = $db->query($q);
 while ($area = $db_result->fetch_row()) {
 	$data[] = array($area[0], (float)$area[1], '<div class="chart-tooltip"><span class="area-title">' . $area[2] . '</span><br><span class="area-avg-label">' . $area[3] . '</span><br><span class="area-avg-value">' . $area[1] . "</span></div>");
